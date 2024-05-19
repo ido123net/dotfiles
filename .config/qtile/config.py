49360c1b838f812107f03cb9713fc12bd0793d87
Ido Frenkel <ido123net@gmail.com>
2024-05-19 23:53:47 +0300

    Move bar to the top

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -160,7 +160,7 @@ extension_defaults = widget_defaults.copy()
 
 screens = [
     Screen(
-        bottom=bar.Bar(
+        top=bar.Bar(
             [
                 widget.CurrentLayout(),
                 widget.GroupBox(),
